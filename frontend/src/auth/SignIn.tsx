717a08a45eb990b04e65ff7e263ab4878594ddc4
DolenDeori <dulench.deori564@gmail.com>
2025-02-24 20:15:27 +0530

    feat: add SignIn and SignUp components for user authentication

---
+++ frontend/src/auth/SignIn.tsx
@@ -0,0 +1,44 @@
+import { NavLink } from "react-router";
+
+const SignIn = () => {
+  return (
+    <div className="h-svh w-full flex flex-col justify-center items-center bg-gray-200">
+      <h1 className="font-bold text-3xl">Sign In</h1>
+      <form action="post" className="mt-5 space-y-2">
+        <div className="flex flex-col gap-y-2">
+          <label htmlFor="">Email</label>
+          <input
+            className="border p-2 outline-none"
+            type="email"
+            name="full_name"
+            placeholder="Your Full Name"
+          />
+        </div>
+        <div className="flex flex-col gap-y-2">
+          <label htmlFor="">Password</label>
+          <input
+            className="border p-2 outline-none"
+            type="password"
+            name="password"
+            placeholder="Your Full Name"
+          />
+        </div>
+      </form>
+      <button
+        type="submit"
+        className="mt-5 cursor-pointer bg-blue-500 text-white p-2 px-8 rounded"
+      >
+        Create Account
+      </button>
+
+      <p className="mt-5">
+        Don't have an account?{" "}
+        <NavLink to="/auth/signup" className="font-bold text-blue-500">
+          Sign Up
+        </NavLink>
+      </p>
+    </div>
+  );
+};
+
+export default SignIn;
