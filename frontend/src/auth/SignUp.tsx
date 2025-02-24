326bbb4d8ab0c6ad6e6d71b889f13beca5de9f9d
Pragyan <youremail@example.com>
2025-02-25 02:24:28 +0530

    feat: backend added till auth

--- frontend/src/auth/SignUp.tsx
+++ frontend/src/auth/SignUp.tsx
@@ -1,17 +1,52 @@
+import { useState } from "react";
 import { NavLink } from "react-router";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    
+    try {
+      const response = await fetch("http://localhost:5500/api/v1/auth", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert("User registered successfully!");
+      } else {
+        alert(data.message || "Error occurred!");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
     
   return (
     <div className="h-svh w-full flex flex-col justify-center items-center bg-gray-200">
       <h1 className="font-bold text-3xl">Sign Up</h1>
-      <form action="post" className="mt-5 space-y-2">
+      <form action="post" className="mt-5 space-y-2" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-y-2">
           <label htmlFor="">Name</label>
           <input
             className="border p-2 outline-none"
             type="text"
-            name="full_name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
             placeholder="Your Full Name"
           />
         </div>
@@ -20,7 +55,9 @@ const SignUp = () => {
           <input
             className="border p-2 outline-none"
             type="email"
-            name="full_name"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
             placeholder="Your Full Name"
           />
         </div>
@@ -30,6 +67,8 @@ const SignUp = () => {
             className="border p-2 outline-none"
             type="password"
             name="password"
+            value={formData.password}
+            onChange={handleChange}
             placeholder="Your Full Name"
           />
         </div>
@@ -38,6 +77,8 @@ const SignUp = () => {
           <input
             className="border p-2 outline-none"
             type="password"
+            value={formData.password}
+            onChange={handleChange}
             name="confirm_password"
             placeholder="Your Full Name"
           />
