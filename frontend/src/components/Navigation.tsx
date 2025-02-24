8891734364293f6acd005474f31fb2cecab9ed4c
DolenDeori <dulench.deori564@gmail.com>
2025-02-24 20:15:34 +0530

    feat: add Navigation component for site navigation

---
+++ frontend/src/components/Navigation.tsx
@@ -0,0 +1,33 @@
+import { NavLink } from "react-router";
+
+const Navigation = () => {
+  return (
+    <header>
+      <nav className="flex justify-between items-center px-32 py-4 bg-gray-300">
+        <div>
+          <NavLink to="/" end>
+            Hello Books
+          </NavLink>
+        </div>
+        <ul className="flex gap-x-6">
+          <li>
+            <NavLink to="/" end>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/auth/signin">Sign In</NavLink>
+          </li>
+          <li>
+            <NavLink to="/auth/signup">Sign Up</NavLink>
+          </li>
+          <li>
+            <NavLink to="/auth/profile">Profile</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navigation;
