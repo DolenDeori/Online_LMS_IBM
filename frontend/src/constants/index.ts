11765ebc7dd3d8dd319c467f36d39b72e50b270e
DolenDeori <dulench.deori564@gmail.com>
2025-02-25 23:37:56 +0530

    feat: add hero images and cover images with star ratings to book data

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,3 +1,13 @@
+import hero_image from "../assets/images/hero_images.jpg";
+import hero_image2 from "../assets/images/hero_image.jpg";
+import logo from "../assets/images/logo.png";
+
+export const images = {
+  hero_image,
+  hero_image2,
+  logo,
+};
+
 export const books = [
   {
     id: 1,
@@ -6,6 +16,9 @@ export const books = [
     publishDate: "1925-04-10",
     pages: 180,
     available: true,
+    starReview: 4.5,
+    coverImage:
+      "https://images.rawpixel.com/image_1100/cHJpdmF0ZS9zdGF0aWMvZmlsZXMvd2Vic2l0ZS8yMDIzLTAzL2NsYXJrMTk5My0tMTktaW1hZ2UuanBn.jpg",
   },
   {
     id: 2,
@@ -14,6 +27,9 @@ export const books = [
     publishDate: "1960-07-11",
     pages: 281,
     available: false,
+    starReview: 4.8,
+    coverImage:
+      "https://images.rawpixel.com/image_social_portrait/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA5L21ldDM5NzA5Mi1pbWFnZS5qcGc.jpg",
   },
   {
     id: 3,
@@ -22,6 +38,9 @@ export const books = [
     publishDate: "1949-06-08",
     pages: 328,
     available: true,
+    starReview: 4.6,
+    coverImage:
+      "https://images.rawpixel.com/image_1100/cHJpdmF0ZS9zdGF0aWMvZmlsZXMvd2Vic2l0ZS8yMDIzLTAzL2NsYXJrMTk5My0tMTktaW1hZ2UuanBn.jpg",
   },
   {
     id: 4,
@@ -30,6 +49,9 @@ export const books = [
     publishDate: "1813-01-28",
     pages: 279,
     available: true,
+    starReview: 4.7,
+    coverImage:
+      "https://images.rawpixel.com/image_1100/cHJpdmF0ZS9zdGF0aWMvZmlsZXMvd2Vic2l0ZS8yMDIzLTAzL2NsYXJrMTk5My0tMTktaW1hZ2UuanBn.jpg",
   },
   {
     id: 5,
@@ -38,6 +60,9 @@ export const books = [
     publishDate: "1951-07-16",
     pages: 214,
     available: false,
+    starReview: 4.3,
+    coverImage:
+      "https://images.rawpixel.com/image_1100/cHJpdmF0ZS9zdGF0aWMvZmlsZXMvd2Vic2l0ZS8yMDIzLTAzL2NsYXJrMTk5My0tMTktaW1hZ2UuanBn.jpg",
   },
   {
     id: 6,
@@ -46,6 +71,9 @@ export const books = [
     publishDate: "1851-10-18",
     pages: 635,
     available: true,
+    starReview: 4.0,
+    coverImage:
+      "https://images.rawpixel.com/image_1100/cHJpdmF0ZS9zdGF0aWMvZmlsZXMvd2Vic2l0ZS8yMDIzLTAzL2NsYXJrMTk5My0tMTktaW1hZ2UuanBn.jpg",
   },
   {
     id: 7,
@@ -54,6 +82,9 @@ export const books = [
     publishDate: "1869-01-01",
     pages: 1225,
     available: false,
+    starReview: 4.9,
+    coverImage:
+      "https://images.rawpixel.com/image_1100/cHJpdmF0ZS9zdGF0aWMvZmlsZXMvd2Vic2l0ZS8yMDIzLTAzL2NsYXJrMTk5My0tMTktaW1hZ2UuanBn.jpg",
   },
   {
     id: 8,
@@ -62,6 +93,9 @@ export const books = [
     publishDate: "1937-09-21",
     pages: 310,
     available: true,
+    starReview: 4.8,
+    coverImage:
+      "https://images.rawpixel.com/image_1100/cHJpdmF0ZS9zdGF0aWMvZmlsZXMvd2Vic2l0ZS8yMDIzLTAzL2NsYXJrMTk5My0tMTktaW1hZ2UuanBn.jpg",
   },
   {
     id: 9,
@@ -70,6 +104,9 @@ export const books = [
     publishDate: "1866-01-01",
     pages: 430,
     available: false,
+    starReview: 4.7,
+    coverImage:
+      "https://images.rawpixel.com/image_1100/cHJpdmF0ZS9zdGF0aWMvZmlsZXMvd2Vic2l0ZS8yMDIzLTAzL2NsYXJrMTk5My0tMTktaW1hZ2UuanBn.jpg",
   },
   {
     id: 10,
@@ -78,6 +115,9 @@ export const books = [
     publishDate: "1954-07-29",
     pages: 1178,
     available: true,
+    starReview: 4.9,
+    coverImage:
+      "https://images.rawpixel.com/image_1100/cHJpdmF0ZS9zdGF0aWMvZmlsZXMvd2Vic2l0ZS8yMDIzLTAzL2NsYXJrMTk5My0tMTktaW1hZ2UuanBn.jpg",
   },
   {
     id: 11,
@@ -86,6 +126,9 @@ export const books = [
     publishDate: "1932-08-18",
     pages: 268,
     available: true,
+    starReview: 4.6,
+    coverImage:
+      "https://images.rawpixel.com/image_1100/cHJpdmF0ZS9zdGF0aWMvZmlsZXMvd2Vic2l0ZS8yMDIzLTAzL2NsYXJrMTk5My0tMTktaW1hZ2UuanBn.jpg",
   },
   {
     id: 12,
@@ -94,6 +137,9 @@ export const books = [
     publishDate: "1847-10-16",
     pages: 500,
     available: false,
+    starReview: 4.5,
+    coverImage:
+      "https://images.rawpixel.com/image_1100/cHJpdmF0ZS9zdGF0aWMvZmlsZXMvd2Vic2l0ZS8yMDIzLTAzL2NsYXJrMTk5My0tMTktaW1hZ2UuanBn.jpg",
   },
   {
     id: 13,
@@ -102,6 +148,9 @@ export const books = [
     publishDate: "1847-12-17",
     pages: 416,
     available: true,
+    starReview: 4.4,
+    coverImage:
+      "https://images.rawpixel.com/image_1100/cHJpdmF0ZS9zdGF0aWMvZmlsZXMvd2Vic2l0ZS8yMDIzLTAzL2NsYXJrMTk5My0tMTktaW1hZ2UuanBn.jpg",
   },
   {
     id: 14,
@@ -110,6 +159,9 @@ export const books = [
     publishDate: "1988-01-01",
     pages: 208,
     available: false,
+    starReview: 4.3,
+    coverImage:
+      "https://images.rawpixel.com/image_1100/cHJpdmF0ZS9zdGF0aWMvZmlsZXMvd2Vic2l0ZS8yMDIzLTAzL2NsYXJrMTk5My0tMTktaW1hZ2UuanBn.jpg",
   },
   {
     id: 15,
@@ -118,6 +170,9 @@ export const books = [
     publishDate: "-800-01-01",
     pages: 541,
     available: true,
+    starReview: 4.8,
+    coverImage:
+      "https://images.rawpixel.com/image_1100/cHJpdmF0ZS9zdGF0aWMvZmlsZXMvd2Vic2l0ZS8yMDIzLTAzL2NsYXJrMTk5My0tMTktaW1hZ2UuanBn.jpg",
   },
   {
     id: 16,
@@ -126,6 +181,9 @@ export const books = [
     publishDate: "1862-01-01",
     pages: 1463,
     available: false,
+    starReview: 4.7,
+    coverImage:
+      "https://images.rawpixel.com/image_1100/cHJpdmF0ZS9zdGF0aWMvZmlsZXMvd2Vic2l0ZS8yMDIzLTAzL2NsYXJrMTk5My0tMTktaW1hZ2UuanBn.jpg",
   },
   {
     id: 17,
@@ -134,6 +192,9 @@ export const books = [
     publishDate: "1897-05-26",
     pages: 418,
     available: true,
+    starReview: 4.6,
+    coverImage:
+      "https://images.rawpixel.com/image_1100/cHJpdmF0ZS9zdGF0aWMvZmlsZXMvd2Vic2l0ZS8yMDIzLTAzL2NsYXJrMTk5My0tMTktaW1hZ2UuanBn.jpg",
   },
   {
     id: 18,
@@ -142,6 +203,9 @@ export const books = [
     publishDate: "1818-01-01",
     pages: 280,
     available: false,
+    starReview: 4.5,
+    coverImage:
+      "https://images.rawpixel.com/image_1100/cHJpdmF0ZS9zdGF0aWMvZmlsZXMvd2Vic2l0ZS8yMDIzLTAzL2NsYXJrMTk5My0tMTktaW1hZ2UuanBn.jpg",
   },
   {
     id: 19,
@@ -150,6 +214,9 @@ export const books = [
     publishDate: "1890-07-01",
     pages: 254,
     available: true,
+    starReview: 4.4,
+    coverImage:
+      "https://images.rawpixel.com/image_1100/cHJpdmF0ZS9zdGF0aWMvZmlsZXMvd2Vic2l0ZS8yMDIzLTAzL2NsYXJrMTk5My0tMTktaW1hZ2UuanBn.jpg",
   },
   {
     id: 20,
@@ -158,5 +225,8 @@ export const books = [
     publishDate: "1878-01-01",
     pages: 864,
     available: false,
+    starReview: 4.7,
+    coverImage:
+      "https://images.rawpixel.com/image_1100/cHJpdmF0ZS9zdGF0aWMvZmlsZXMvd2Vic2l0ZS8yMDIzLTAzL2NsYXJrMTk5My0tMTktaW1hZ2UuanBn.jpg",
   },
 ];
