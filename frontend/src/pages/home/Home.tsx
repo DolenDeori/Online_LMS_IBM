fbeaf51a4b2084585af0dc024bcb21bfef6241e3
DolenDeori <dulench.deori564@gmail.com>
2025-02-25 23:37:27 +0530

    feat: update Home component layout and integrate SearchNav for improved book display

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,36 +1,31 @@
+import SearchNav from "@/components/SearchNav";
 import { books } from "@/constants";
 
-const Home = () => {
+const Home = ({ setBookId }: { setBookId: any }) => {
   return (
-    <main>
-      <section className="h-[60svh] flex flex-col items-center justify-center bg-gray-500">
-        <h1 className="text-3xl text-white">Home</h1>
-        <p className="text-3xl text-white">Welcome to the home page</p>
-      </section>
-      <section className="p-4 px-8">
-        <h1 className="text-2xl font-semibold text-center">
-          Our Book Collection
-        </h1>
-        <div className="gid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
-          {
-            // Show book list
-            books.map((book) => (
-              <div
-                key={book.id}
-                className="flex items-center justify-between p-4 border-b"
-              >
-                <div>
-                  <h2 className="text-xl font-semibold">{book.title}</h2>
-                  <p className="text-lg">{book.author}</p>
-                </div>
-                <div>
-                  <button className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer">
-                    Borrow
-                  </button>
-                </div>
+    <main className="bg-blue-200 h-svh overflow-y-scroll">
+      <SearchNav />
+      <section className="p-4">
+        <h1>Popular Books</h1>
+
+        <div className="grid grid-cols-5 gap-6 mt-6 gap-y-8">
+          {books.map((book) => (
+            <div
+              key={book.id}
+              className="cursor-pointer"
+              onClick={() => setBookId(book.id)}
+            >
+              <div>
+                <img
+                  src={book.coverImage}
+                  alt="book cover image"
+                  className="p-1 border-2 h-[200px] w-full object-cover object-top"
+                />
               </div>
-            ))
-          }
+              <h2 className="font-semibold text-sm">{book.title}</h2>
+              <p className="text-xs">{book.author}</p>
+            </div>
+          ))}
         </div>
       </section>
     </main>
