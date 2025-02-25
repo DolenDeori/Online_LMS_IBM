78891da188c338f01d900d112b2920c81a66d389
DolenDeori <dulench.deori564@gmail.com>
2025-02-25 23:37:41 +0530

    feat: integrate BookInfo component and manage book selection state in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
+import BookInfo from "./components/BookInfo";
 import Navigation from "./components/Navigation";
 import Home from "./pages/home/Home";
 
 function App() {
+  const [bookInfo, setBookInfo] = useState(0);
   return (
-    <>
-      <Navigation />
-      <Home />
-    </>
+    <div className="flex h-svh">
+      <div>
+        <Navigation />
+      </div>
+      <div className="flex-2 relative">
+        <Home setBookId={setBookInfo} />
+      </div>
+      <div className="flex-1">
+        <BookInfo bookId={bookInfo} />
+      </div>
+    </div>
   );
 }
 
