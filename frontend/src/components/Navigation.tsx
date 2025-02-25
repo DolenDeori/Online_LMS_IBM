6ebb4ab625c69b5638b6223b930779a679c8ab6f
DolenDeori <dulench.deori564@gmail.com>
2025-02-25 23:37:08 +0530

    feat: enhance Navigation component with new layout and icons

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,31 +1,86 @@
+import { images } from "@/constants";
+import {
+  Squares2X2Icon,
+  BookmarkIcon,
+  ChatBubbleLeftRightIcon,
+  ArrowRightEndOnRectangleIcon,
+  UserGroupIcon,
+} from "@heroicons/react/24/outline";
+import { HomeIcon } from "@heroicons/react/24/solid";
 import { NavLink } from "react-router";
-
 
 const Navigation = () => {
   return (
     <header>
-      <nav className="flex justify-between items-center px-32 py-4 bg-gray-300">
+      <nav className="py-6 px-2 h-svh flex flex-col justify-between items-center bg-blue-100">
         <div>
-          <NavLink to="/" end>
-            Hello Books
+          <NavLink to="/" className="flex items-center justify-center" end>
+            <img src={images.logo} alt="" className="h-10" />
           </NavLink>
+          <div className="mt-16">
+            <ul className="flex flex-col gap-8">
+              <li>
+                <NavLink
+                  to="#"
+                  className="text-xs font-medium flex flex-col items-center justify-center gap-2 text-blue-800"
+                >
+                  <HomeIcon className="h-7 w-7" />
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="#"
+                  className="text-xs font-medium flex flex-col items-center justify-center gap-2"
+                >
+                  <Squares2X2Icon className="h-7 w-7" />
+                  Category
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="#"
+                  className="text-xs font-medium flex flex-col items-center justify-center gap-2"
+                >
+                  <BookmarkIcon className="h-7 w-7" />
+                  Saved
+                </NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
-        <ul className="flex gap-x-6">
-          <li>
-            <NavLink to="/" end>
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/auth/signin">Sign In</NavLink>
-          </li>
-          <li>
-            <NavLink to="/auth/signup">Sign Up</NavLink>
-          </li>
-          <li>
-            <NavLink to="/auth/profile">Profile</NavLink>
-          </li>
-        </ul>
+
+        <div>
+          <ul className="flex flex-col gap-8">
+            <li>
+              <NavLink
+                to="#"
+                className="text-xs font-medium flex flex-col items-center justify-center gap-2"
+              >
+                <UserGroupIcon className="h-7 w-7" />
+                About Us
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="#"
+                className="text-xs font-medium flex flex-col items-center justify-center gap-2"
+              >
+                <ChatBubbleLeftRightIcon className="h-7 w-7" />
+                Contact
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="#"
+                className="text-xs font-medium flex flex-col items-center justify-center gap-2"
+              >
+                <ArrowRightEndOnRectangleIcon className="h-7 w-7" />
+                Login In
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
