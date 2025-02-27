24ad898ad5f5bd3d98ee9499b379be47e9f4e4c4
DolenDeori <dulench.deori564@gmail.com>
2025-02-28 00:34:31 +0530

    feat: remove debug log from BookInfo component

--- frontend/src/pages/BookInfo.tsx
+++ frontend/src/pages/BookInfo.tsx
@@ -5,7 +5,6 @@ const BookInfo = () => {
   const { id } = useParams();
 
   const book = books.find((book) => book.id.toString() === id);
-  console.log("The book is", book);
 
   if (!book) {
     return <div>Book not found</div>;
