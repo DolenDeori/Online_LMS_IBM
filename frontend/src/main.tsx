386ec8eced89831f59c6fb430a3841bff52d19bb
DolenDeori <dulench.deori564@gmail.com>
2025-02-27 23:58:39 +0530

    feat: simplify routing by integrating App component directly in BrowserRouter

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,21 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter, Routes, Route } from "react-router";
-import SignIn from "./auth/SignIn.tsx";
-import SignUp from "./auth/SignUp.tsx";
-import Profile from "./auth/Profile.tsx";
+import { BrowserRouter } from "react-router";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/auth/signin" element={<SignIn />} />
-        <Route path="/auth/signup" element={<SignUp />} />
-        <Route path="/auth/profile" element={<Profile />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
