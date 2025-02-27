22422e58745c4f348177452dd7d722d34f1d6cb9
DolenDeori <dulench.deori564@gmail.com>
2025-02-28 00:34:17 +0530

    feat: enhance Navigation component with hover effects and improved styling

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,80 +1,105 @@
 import { images } from "@/constants";
-import { NavLink } from "react-router";
+import { useState } from "react";
+import { NavLink, useLocation } from "react-router";
 
 const Navigation = () => {
+  const location = useLocation();
+  const [showNavLink, setShowNavLink] = useState<string | null>(null);
   return (
     <header>
       {/* ------------ navigation bar start ------------------- */}
-      <nav className="py-8 px-2 lg:px-3 border-r-2 h-svh flex flex-col justify-between items-center font-funnel bg-blue-100">
+      <nav className="py-8 px-2 lg:px-4 border-r border-gray-300 h-svh flex flex-col justify-between items-center font-funnel">
         <div>
           <NavLink to="/" className="flex items-center justify-center" end>
-            <img src={images.logo} alt="image logo" className="h-10" />
+            <img
+              src={images.logo_icon_white}
+              alt="image logo"
+              className={`h-10 bg-blue-700 p-2 rounded-full hover:bg-blue-800`}
+            />
           </NavLink>
 
           <div className="mt-8">
-            <ul className="flex flex-col gap-8">
-              <li>
+            <ul className="flex flex-col justify-center items-center gap-y-8">
+              <li
+                className="hover:bg-gray-200 h-12 w-12 flex justify-center items-center rounded-md cursor-pointer"
+                onMouseEnter={() => setShowNavLink("home")}
+                onMouseLeave={() => setShowNavLink(null)}
+              >
                 <NavLink
                   to="/"
-                  className="text-xs font-funnel font-bold flex flex-col items-center justify-center gap-1 text-blue-800"
+                  className="font-funnel font-bold gap-1 text-black"
                 >
-                  <i className="bi bi-house-door-fill text-2xl"></i>
-                  Home
+                  <i
+                    className={`bi bi-house-door${
+                      location.pathname === "/" ? "-fill" : ""
+                    } text-2xl`}
+                  ></i>
+
+                  {showNavLink === "home" && (
+                    <span className="absolute left-17 bg-gray-900 font-funnel font-normal text-white text-sm px-2 py-1 rounded-md z-1">
+                      home
+                    </span>
+                  )}
                 </NavLink>
               </li>
-              <li>
+              <li
+                className="hover:bg-gray-200 h-12 w-12 flex justify-center items-center rounded-md cursor-pointer"
+                onMouseEnter={() => setShowNavLink("explore")}
+                onMouseLeave={() => setShowNavLink(null)}
+              >
                 <NavLink
-                  to="#"
+                  to="/explore"
                   className="text-xs font-funnel font-semibold flex flex-col items-center justify-center gap-1"
                 >
-                  <i className="bi bi-grid text-2xl"></i>
-                  Category
+                  <i
+                    className={`bi bi-compass${
+                      location.pathname === "/explore" ? "-fill" : ""
+                    } text-2xl`}
+                  ></i>
+                  {showNavLink === "explore" && (
+                    <span className="absolute left-17 bg-gray-900 font-funnel font-normal text-white text-sm px-2 py-1 rounded-md z-1">
+                      explore
+                    </span>
+                  )}
                 </NavLink>
               </li>
-              <li>
+              <li
+                className="hover:bg-gray-200 h-12 w-12 flex justify-center items-center rounded-md cursor-pointer"
+                onMouseEnter={() => setShowNavLink("saved")}
+                onMouseLeave={() => setShowNavLink(null)}
+              >
                 <NavLink
-                  to="#"
+                  to="/saved"
                   className="text-xs font-funnel font-semibold flex flex-col items-center justify-center gap-1"
                 >
-                  <i className="bi bi-bookmark text-2xl"></i>
-                  Saved
+                  <i
+                    className={`bi bi-bookmark${
+                      location.pathname === "/saved" ? "-fill" : ""
+                    } text-2xl`}
+                  ></i>
+                  {showNavLink === "saved" && (
+                    <span className="absolute left-17 bg-gray-900 font-funnel font-normal text-white text-sm px-2 py-1 rounded-md z-1">
+                      saved
+                    </span>
+                  )}
                 </NavLink>
               </li>
             </ul>
           </div>
         </div>
 
-        <div>
+        {/* <div>
           <ul className="flex flex-col gap-8">
-            <li>
-              <NavLink
-                to="#"
-                className="text-xs font-semibold flex flex-col items-center justify-center gap-1"
-              >
-                <i className="bi bi-people text-2xl"></i>
-                About Us
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="#"
-                className="text-xs font-semibold flex flex-col items-center justify-center gap-1"
-              >
-                <i className="bi bi-chat-left text-2xl"></i>
-                Contact
-              </NavLink>
-            </li>
             <li>
               <NavLink
                 to="/auth/signin"
                 className="text-xs font-semibold flex flex-col items-center justify-center gap-1"
               >
                 <i className="bi bi-box-arrow-in-right text-2xl"></i>
-                Login In
               </NavLink>
             </li>
           </ul>
-        </div>
+        </div> */}
       </nav>
       {/* ------------ navigation bar end ------------------- */}
     </header>
