f55cb9a7c2d1db48089fe397bee2cc181f4b4b0f
DolenDeori <dulench.deori564@gmail.com>
2025-02-27 23:58:25 +0530

    feat: add logo icon images for enhanced branding

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,7 +1,11 @@
 import logo from "../assets/images/logo.png";
+import logo_icon from "../assets/images/Logo_Icon.png";
+import logo_icon_white from "../assets/images/Logo_Icon_White.png";
 
 export const images = {
   logo,
+  logo_icon,
+  logo_icon_white,
 };
 
 export const books = [
