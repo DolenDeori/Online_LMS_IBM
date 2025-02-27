5fbdff68a90a120ec0ab52abebd368f5c372411f
DolenDeori <dulench.deori564@gmail.com>
2025-02-28 01:55:14 +0530

    feat: enhance Home component with improved layout and book details display

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { books } from "@/constants";
-import { useNavigate } from "react-router";
+import { NavLink, useNavigate } from "react-router";
 import SearchNav from "@/components/SearchNav";
 
 const Home = () => {
@@ -12,19 +12,26 @@ const Home = () => {
         <section className="p-4">
           <h1 className="text-xl font-semibold">Our Book Collections</h1>
 
-          <div className="lg:columns-6 md:columns-3 md:space-y-12 space-y-6 lg:space-y-6 mt-5 mb-5">
+          <div className="lg:columns-6 md:columns-3 md:space-y-12 space-y-6 lg:space-y-12 mt-5 mb-5">
             {books.map((book) => (
               <div
                 key={book.id}
                 className="cursor-pointer"
                 onClick={() => navigate(`/books/${book.id}`)}
               >
-                <div>
+                <div className="bg-black rounded-xl overflow-hidden relative">
                   <img
                     src={book.coverImage}
                     alt="book cover image"
-                    className=" w-full rounded-md"
+                    className=" w-full rounded-xl hover:opacity-70"
                   />
+                </div>
+                <div>
+                  <h2 className="text-gray-800">{book.title}</h2>
+                  <p className="flex items-center gap-1 text-sm text-gray-600">
+                    {book.starReview}{" "}
+                    <i className="bi bi-star-fill text-xs"></i>
+                  </p>
                 </div>
               </div>
             ))}
