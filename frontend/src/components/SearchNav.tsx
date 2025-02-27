2d6890efab1b7a420681a01485055866806e44c4
DolenDeori <dulench.deori564@gmail.com>
2025-02-28 00:34:24 +0530

    feat: enhance SearchNav component with sign-up and sign-in links

--- frontend/src/components/SearchNav.tsx
+++ frontend/src/components/SearchNav.tsx
@@ -1,16 +1,29 @@
+import { NavLink } from "react-router";
+
 const SearchNav = () => {
   return (
-    <div className="bg-blue-600 p-4 w-full z-10">
-      <div>
-        <h1 className="text-md text-white mb-2">Search your fave book</h1>
-      </div>
-      <div className="bg-white flex justify-between items-center rounded-xl p-1 px-2 border-2">
+    <div className="bg-blue-600 p-4 z-10 flex justify-between items-center gap-4 fixed">
+      <div className="bg-white flex justify-between items-center rounded-md p-1 px-2 flex-2">
         <i className="bi bi-search text-xl"></i>
         <input
           type="text"
           className="bg-white w-full outline-none p-2 text-sm"
           placeholder="Title, Author, Host or Topic ..."
         />
+      </div>
+      <div className="flex">
+        <NavLink
+          to="/auth/signup"
+          className="p-2.5 bg-white text-blue-700 rounded-l-md border-r-2"
+        >
+          Sign Up
+        </NavLink>
+        <NavLink
+          to="/auth/signin"
+          className="p-2.5 bg-white text-blue-700 rounded-r-md"
+        >
+          Sign In
+        </NavLink>
       </div>
     </div>
   );
