09fe7c6888d8268bb0385aa00a4b101d952c4ca6
DolenDeori <dulench.deori564@gmail.com>
2025-02-28 02:59:17 +0530

    feat: update routing in App component to include Profile and SearchNav components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,23 +6,29 @@ import SignIn from "./auth/SignIn";
 import SignUp from "./auth/SignUp";
 import Explore from "./pages/Explore";
 import Saved from "./pages/Saved";
+import SearchNav from "./components/SearchNav";
+import Profile from "./auth/Profile";
 
 function App() {
   const location = useLocation();
 
   const hideNavigation = location.pathname.startsWith("/auth");
+  // const hideSearchNavigation = location.pathname.startsWith("/home");
 
   return (
     <div className="flex h-screen">
       {!hideNavigation && <Navigation />}
       <div className="flex-1 relative overflow-y-scroll">
+        {!hideNavigation && <SearchNav />}
+
         <Routes>
           <Route index element={<Home />} />
-          <Route path="/explore" element={<Explore />} />
-          <Route path="/saved" element={<Saved />} />
-          <Route path="/books/:id" element={<BookInfo />} />
+          <Route path="/home/explore" element={<Explore />} />
+          <Route path="/home/saved" element={<Saved />} />
+          <Route path="/home/books/:id" element={<BookInfo />} />
           <Route path="/auth/signin" element={<SignIn />} />
           <Route path="/auth/signup" element={<SignUp />} />
+          <Route path="/auth/profile" element={<Profile />} />
         </Routes>
       </div>
     </div>
