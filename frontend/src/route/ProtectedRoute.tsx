92cfaf93fafd128e00883f928714a68e55a1ae19
DolenDeori <dulench.deori564@gmail.com>
2025-02-28 09:59:34 +0530

    feat: add ProtectedRoute component for route access control based on authentication token

---
+++ frontend/src/route/ProtectedRoute.tsx
@@ -0,0 +1,10 @@
+import { Navigate, Outlet } from "react-router";
+
+const ProtectedRoute = () => {
+  const token = localStorage.getItem("token");
+  console.log("This is a token", token);
+
+  return token ? <Outlet /> : <Navigate to="/auth/signin" replace />;
+};
+
+export default ProtectedRoute;
