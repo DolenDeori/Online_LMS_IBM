cf7777beed69e0656c9bcf77c12493b59b1ba306
DolenDeori <dulench.deori564@gmail.com>
2025-03-02 21:40:12 +0530

    feat: add axios dependency and update environment variables for book API endpoints

--- backend/app.js
+++ backend/app.js
@@ -1,46 +1,41 @@
-import express from 'express';
-import { PORT } from './config/env.js';
-import  userRouter  from './routes/user.routes.js'
-import  authRouter from './routes/auth.routes.js'
-import connectToDatabase from './database/mongodb.js';
-import errorMiddleware from './middleware/error.middleware.js';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import { PORT } from "./config/env.js";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middleware/error.middleware.js";
+import cookieParser from "cookie-parser";
 import cors from "cors";
-import bookRouter from './routes/book.routes.js'
-import borrowRouter from './routes/borrow.routes.js';
-
+import bookRouter from "./routes/book.routes.js";
+import borrowRouter from "./routes/borrow.routes.js";
 
 const app = express();
 
-
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
 //Middleware
-app.use(cors({ origin: "http://localhost:5174", credentials: true }));
-
-
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 //in-built express middlewares
-app.use(express.json());    // Ensure JSON parsing is enabled
-app.use(express.urlencoded({extended : false}));  //to handle html form data
-app.use(cookieParser());  //read cookies from incoming reqs adn store user data
+app.use(express.json()); // Ensure JSON parsing is enabled
+app.use(express.urlencoded({ extended: false })); //to handle html form data
+app.use(cookieParser()); //read cookies from incoming reqs adn store user data
 
 //router-level middleware because it handles requests at a specific route path.
-app.use('/api/v1/auth' ,authRouter );
-app.use('/api/v1/users' ,userRouter );
-app.use('/api/v1/books' , bookRouter );
-app.use('/api/v1/books' , borrowRouter );
-
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/books", bookRouter);
+app.use("/api/v1/books", borrowRouter);
 
 //Middlewares
-app.use(errorMiddleware);  //custom
+app.use(errorMiddleware); //custom
 
-app.get('/' , (req,res)=> {
-res.send("Welcome to the Subscription Tracker API!");
+app.get("/", (req, res) => {
+  res.send("Welcome to the Subscription Tracker API!");
 });
 
-app.listen(PORT , async () =>{
-    console.log(`Gulumulu is running on http://localhost:${PORT}`);
-    await connectToDatabase();
-  
-})
+app.listen(PORT, async () => {
+  console.log(`Gulumulu is running on http://localhost:${PORT}`);
+  await connectToDatabase();
+});
 
 export default app;
